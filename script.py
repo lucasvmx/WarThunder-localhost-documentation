f0860d429a563ff08d3835a2455b122cb33c6807
Lucas Vieira <lucas.engen.cc@gmail.com>
2023-07-29 20:51:20 -0300

    chore: saves data to another folder
    Signed-off-by: Lucas Vieira <lucas.engen.cc@gmail.com>

--- script.py
+++ script.py
@@ -2,9 +2,11 @@
 # Python script to get localhost JSON data and transform into markdown fields
 #
 
+import json
+import os
+import os.path
 import urllib
 import urllib.request
-import json
 from sys import exit
 
 # URL constants
@@ -28,89 +30,98 @@ state_file = 'state.md'
 
 # Type constants
 wt_data_types = {
-    "GAMECHAT": { "URL": gamechat_url, "FILE": gamechat_file},
-    "HUDMSG": { "URL": hudmsg_url, "FILE": hudmsg_file},
-    "INDICATORS": { "URL": indicators_url, "FILE": indicators_file },
-    "MAPOBJECTS": { "URL": mapobjects_url, "FILE": mapobjects_file },
-    "MAPINFO": { "URL": mapinfo_url, "FILE": mapinfo_file },
-    "MISSION": { "URL": mission_url, "FILE": mission_file },
-    "STATE": { "URL": state_url, "FILE": state_file }
+	"GAMECHAT": { "URL": gamechat_url, "FILE": gamechat_file},
+	"HUDMSG": { "URL": hudmsg_url, "FILE": hudmsg_file},
+	"INDICATORS": { "URL": indicators_url, "FILE": indicators_file },
+	"MAPOBJECTS": { "URL": mapobjects_url, "FILE": mapobjects_file },
+	"MAPINFO": { "URL": mapinfo_url, "FILE": mapinfo_file },
+	"MISSION": { "URL": mission_url, "FILE": mission_file },
+	"STATE": { "URL": state_url, "FILE": state_file }
 }
 
 def get_correct_typename(typename: str):
-    v = {
-        "str": "string",
-        "bool": "boolean",
-        "int": "integer",
-        "float": "decimal"
-    }
+	v = {
+		"str": "string",
+		"bool": "boolean",
+		"int": "integer",
+		"float": "decimal"
+	}
 
-    for k in v.keys():
-        if k == typename:
-            return v[f'{k}']
+	for k in v.keys():
+		if k == typename:
+			return v[f'{k}']
 
-    return typename
+	return typename
 
 def get_url_and_filename(data_type: str):
-    if not data_type in wt_data_types.keys():
-        raise("invalid key")
+	if not data_type in wt_data_types.keys():
+		raise("invalid key")
 
-    return [wt_data_types[data_type]["URL"], wt_data_types[data_type]["FILE"]]
+	return [wt_data_types[data_type]["URL"], wt_data_types[data_type]["FILE"]]
 
 def get_wt_data(data_type: str):
 
-    data: bytes = b''
+	data: bytes = b''
 
-    # Extract data
-    try:
-        global url, filename
-        url, filename = get_url_and_filename(data_type)
-    except Exception as e:
-        print("could not get url and filename: {}".format(e))
-        exit(1)
+	output_folder = "OUTPUT"
 
-    data: bytes = b''
+	try:
+		os.mkdir(output_folder)
+	except:
+		pass
 
-    # Do HTTP request
-    try:
-        with urllib.request.urlopen(url) as f:
-            print("requesting {}".format(url))
-            data += f.read()
-    except:
-        print("failed to get resource: {}".format(url))
-        return
+	# Extract data
+	try:
+		global url, filename
+		url, filename = get_url_and_filename(data_type)
+	except Exception as e:
+		print("could not get url and filename: {}".format(e))
+		exit(1)
 
-    payload = data.decode('utf-8')
-    json_info = list[str](json.loads(payload))
+	data: bytes = b''
 
-    # Write result to markdown
-    with open(filename, "w") as file:
-        index = 0
-        for k in json_info:
-            datatype = str(type(json_info[index]))
-            start = datatype.find('\'')
-            end = datatype.find('\'', start + 1)
-            if end == -1:
-                print('error')
-                continue
+	# Do HTTP request
+	try:
+		with urllib.request.urlopen(url) as f:
+			print("requesting {}".format(url))
+			data += f.read()
+	except:
+		print("failed to get resource: {}".format(url))
+		return
 
-            _type = get_correct_typename(datatype[start+1:end:])
-            name = k
+	payload = data.decode('utf-8')
+	json_info = list[str](json.loads(payload))
 
-            markdown = f"""
-    - name: **{name}**
-        * contains: {_type}
-        * description:
-    """
+	# Write result to markdown
+	dest_file = os.path.join(output_folder, filename)
 
-            file.write(markdown)
-            index+=1
+	with open(dest_file, "w") as file:
+		index = 0
+		for k in json_info:
+			datatype = str(type(json_info[index]))
+			start = datatype.find('\'')
+			end = datatype.find('\'', start + 1)
+			if end == -1:
+				print('error')
+				continue
+
+			_type = get_correct_typename(datatype[start+1:end:])
+			name = k
+
+			markdown = f"""
+	- name: **{name}**
+		* contains: {_type}
+		* description:
+	"""
+
+			file.write(markdown)
+			index+=1
 
 def get_all_wt_data():
 
-    for key in wt_data_types.keys():
-        get_wt_data(key)
+	for key in wt_data_types.keys():
+		get_wt_data(key)
 
 
 if __name__ == "__main__":
-    get_all_wt_data()
+	get_all_wt_data()
